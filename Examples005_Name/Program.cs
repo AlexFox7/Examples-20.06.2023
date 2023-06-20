38e70f9670a7e755a9e1d311221cf92fab74d0ac
Alex_Fox <p9776691182@yandex.ru>
2023-06-20 22:21:59 +0300

    программа реакции на определенное значение

---
+++ Examples005_Name/Program.cs
@@ -0,0 +1,13 @@
+﻿// Программа реакции на определенное значение
+Console.WriteLine("Введите имя пользовтеля ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Прости, Маша, мы все проебали...");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
